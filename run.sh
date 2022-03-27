531014bccf21728620fcca0b8aa2d5cad9aff272
BuildTools <kainetrtshadow@gmail.com>
2022-03-27 13:55:18 +0200

    final commit pandas

--- run.sh
+++ run.sh
@@ -1,6 +1,8 @@
-if [[ -d ./.venv ]] ; then
+if [[ -d ./.venv ]]
+then
         source ./.venv/bin/activate
 	alias activate=". ../.env/bin/activate"
 	pipenv install -r requirements.txt
-	python3 main.py
+fi
+python3 main.py
 
