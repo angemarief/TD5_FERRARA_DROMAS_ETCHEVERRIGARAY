1a4255bbc7223e7643aa92365a0b9f2a6cce3ddd
BuildTools <kainetrtshadow@gmail.com>
2022-03-27 14:08:01 +0200

    fixing run.sh

--- run.sh
+++ run.sh
@@ -1,15 +1,7 @@
 if [[ -d ./.venv ]]
 then
-<<<<<<< HEAD
 	source ./.venv/bin/activate
-=======
-        source ./.venv/bin/activate
->>>>>>> pandas
 	alias activate=". ../.env/bin/activate"
 	pipenv install -r requirements.txt
 fi
 python3 main.py
-<<<<<<< HEAD
-=======
-
->>>>>>> pandas
